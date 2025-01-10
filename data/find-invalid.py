f4843ff83778f224fb9f2a4221ecc98fadc1e4a7
paintingofblue <90877067+aprilsbloom@users.noreply.github.com>
2025-01-10 22:45:20 +1300

    fix incorrect source values, add helper for finding, add trial if statement to substance

---
+++ data/find-invalid.py
@@ -0,0 +1,16 @@
+import re
+import json
+
+with open('final_updated_drugs.json', 'r', encoding='utf8') as f:
+		drugs = json.load(f)
+
+for drug in drugs:
+	drug = drugs[drug]
+	# if no substances
+	if not drug.get('sources', {}).get('_general', []):
+		continue
+
+	for substance in drug['sources']['_general']:
+		if not re.match(r'([a-zA-Z0-9-_:\'"\?.,()\[\]\/\\ ]+) - http[s]?:\/\/([a-zA-Z0-9\-%.\/]+)', substance):
+			print(drug['name'])
+			break
