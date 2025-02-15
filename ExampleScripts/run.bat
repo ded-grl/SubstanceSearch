61327b0199166d76617b393a271c698cbda78041
MusicGivesMeLife <guardian451@protonmail.com>
2025-02-15 18:31:17 -0500

    Fixed Windows batch file

--- ExampleScripts/run.bat
+++ ExampleScripts/run.bat
@@ -1,6 +1,7 @@
 ::For cmd.exe
 
-cd ~/SubstanceSearch
-C:\> .venv/Scripts/activate.bat
-python app.py & firefox --new-window http://127.0.0.1:5000
+cd ../
+::venv doesn't seem to work right on Windows
+start python app.py
+"C:\Program Files\Mozilla Firefox\firefox.exe" --new-window 127.0.0.1:5000
 echo "Done!"
